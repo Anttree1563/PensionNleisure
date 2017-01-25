0ac414ff2798b3da782de4be335e9adffadb2346
Anttree1563 <ant_tree@naver.com>
2017-01-25 18:32:37 +0900

    한글키보드 구현
    초성, 중성, 종성 문제

--- app/src/main/java/com/truemind/pensionnleisure/MainActivity.java
+++ app/src/main/java/com/truemind/pensionnleisure/MainActivity.java
@@ -3,13 +3,12 @@ package com.truemind.pensionnleisure;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.truemind.pensionnleisure.softkeyboard.SoftKeyboard;
 
 public class MainActivity extends Activity {
     private final long FINISH_INTERVAL_TIME = 2000;
@@ -22,7 +21,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, Keylogger.class));
+        startService(new Intent(this, SoftKeyboard.class));
         if(namepref!=""||phonepref!="") {
             getPreferences();
             name = namepref;
